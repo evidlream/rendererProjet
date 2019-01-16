64bec8407d221e906b7d7c1f04ffadf3819f9fd4
cael11u <yann.cael4@etu.univ-lorraine.fr>
2019-01-16 08:28:08 +0100

    affichage en fil de fer

--- main.cpp
+++ main.cpp
@@ -18,36 +18,34 @@ std::vector<std::vector<int>> triangles;
 
 void drawLine(int x0, int y0,int x1, int y1, TGAImage &image, TGAColor color){
 
-    bool vertical = false;
-
-
-    if(std::abs(x0-x1) < std::abs(y0-y1)){
-        std::swap(x0,y0);
-        std::swap(x1,y1);
-        vertical = true;
-    }
-
-    if(x0 > x1){
-        std::swap(x1,x0);
-        std::swap(y1,y0);
-    }
-
-    for(int x = x0; x < x1;x++){
-        int t = (x-x0)/(float)(x1-x0);
-        int y = y0*(1.-t)+y1*t;
-
-        if(vertical){
-            image.set(y,x,color);
-        }
-        else{
-            image.set(x,y,color);
-        }
-    }
-
-//correctino a faire : y mal affiché
-
+    bool steep = false;
+       if (std::abs(x0-x1)<std::abs(y0-y1)) {
+           std::swap(x0, y0);
+           std::swap(x1, y1);
+           steep = true;
+       }
+       if (x0>x1) {
+           std::swap(x0, x1);
+           std::swap(y0, y1);
+       }
+       int dx = x1-x0;
+       int dy = y1-y0;
+       int derror2 = std::abs(dy)*2;
+       int error2 = 0;
+       int y = y0;
+       for (int x=x0; x<=x1; x++) {
+           if (steep) {
+               image.set(y, x, color);
+           } else {
+               image.set(x, y, color);
+           }
+           error2 += derror2;
+           if (error2 > dx) {
+               y += (y1>y0?1:-1);
+               error2 -= dx*2;
+           }
+       }
 }
-
 
 std::vector<std::string> explode(std::string const & s, char delimitation)
 {
@@ -56,7 +54,6 @@ std::vector<std::string> explode(std::string const & s, char delimitation)
 
     for (std::string token; std::getline(iss, token, delimitation); )
     {
-        //ajoute delimiation
             res.push_back(std::move(token));
     }
 
@@ -93,21 +90,13 @@ void lectureFichier(std::string name, TGAImage &image){
             streamA >> a;
             std::istringstream streamB(ex[2]);
             streamB >> b;
-            a = a*largeur;
-            b = b*hauteur;
 
-            //on passe tout au dessus de 0
-            if(a < 0){
-                a = a+(largeur/2);
-            }
-            if(b < 0){
-                b = b+(hauteur/2);
-            }
+            a = a*(largeur/2)+(largeur/2);
+            b = b*(hauteur/2)+(hauteur/2);
 
             resultat.push_back(a);
             resultat.push_back(b);
             positions.push_back(resultat);
-                //image.set(a, b, red);
             resultat.clear();
         }
         //recuperations des triangles a tracer
@@ -129,6 +118,26 @@ void lectureFichier(std::string name, TGAImage &image){
     file.close();
 }
 
+void affichagePoint(TGAImage &image){
+    //affichage des points
+    for(int i =0;i < positions.size();i++){
+        image.set(positions[i][0],positions[i][1],white);
+    }
+}
+
+void affichageLignes(TGAImage &image){
+    //dessin des lignes
+    int d,b,c;
+    for(int i = 0;i < triangles.size();i++){
+        d = triangles[i][0]-1;
+        b = triangles[i][1]-1;
+        c = triangles[i][2]-1;
+        drawLine(positions[d][0],positions[d][1],positions[b][0],positions[b][1],image,white);
+        drawLine(positions[d][0],positions[d][1],positions[c][0],positions[c][1],image,white);
+        drawLine(positions[b][0],positions[b][1],positions[c][0],positions[c][1],image,white);
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -138,24 +147,8 @@ int main(int argc, char *argv[])
 
     lectureFichier("../african_head.obj",image);
 
-    //affichage des points
-    for(int i =0;i < positions.size();i++){
-        image.set(positions[i][0],positions[i][1],red);
-    }
+    affichageLignes(image);
 
-
-    //dessin des lignes
-    int d,b,c;
-    /*for(int i = 0;i < triangles.size();i++){
-        d = triangles[i][0];
-        b = triangles[i][1];
-        c = triangles[i][2];
-        drawLine(positions[d][0],positions[d][1],positions[b][0],positions[b][1],image,red);
-        drawLine(positions[d][0],positions[d][1],positions[c][0],positions[c][1],image,red);
-        drawLine(positions[b][0],positions[b][1],positions[c][0],positions[c][1],image,red);
-    }*/
-    image.set(0,0,white);
-    //drawLine(40,20,100,80,image,red);
     image.flip_vertically();
     image.write_tga_file("output.tga");
     return 0;
